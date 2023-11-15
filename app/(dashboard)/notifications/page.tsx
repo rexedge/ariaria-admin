88047a8218b7f8d42ad471c6986de00eee89291c
Jeff AliG <aligwekwe2000j@gmail.com>
2023-11-15 13:05:08 +0100

    Notification workd on

--- app/(dashboard)/notifications/page.tsx
+++ app/(dashboard)/notifications/page.tsx
@@ -1,5 +1,170 @@
 import React from "react";
 
 export default function Notification() {
-  return <div>Notification</div>;
+  return (
+    <div className="flex flex-col gap-2 ml-3 ">
+      <div className="mb-10 font-bold ">Notification</div>
+      <div className="w-full max-w-md py-3 px-8 rounded-lg border bg-white flex justify-between ">
+        <div className="">
+          <div className=" mb-2 ">
+            You have a new order request
+            <span className=" font-bold px-1 ">#456758</span>
+          </div>
+          <div className=""> 12h ago</div>
+        </div>
+        <div className="">
+          <svg
+            width="8"
+            height="8"
+            viewBox="0 0 8 8"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect width="8" height="8" rx="4" fill="#020202" />
+          </svg>
+        </div>
+      </div>
+
+      <div className="w-full max-w-md py-3 px-8 rounded-lg border bg-white flex justify-between">
+        <div className="">
+          <div className="mb-2">
+            You have a new order request
+            <span className="font-bold px-1">#456758</span>
+          </div>
+          <div className=""> 12h ago</div>
+        </div>
+        <div className="">
+          <svg
+            width="8"
+            height="8"
+            viewBox="0 0 8 8"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect width="8" height="8" rx="4" fill="#020202" />
+          </svg>
+        </div>
+      </div>
+      <div className="w-full max-w-md py-3 px-8 rounded-lg border bg-white flex justify-between">
+        <div className="">
+          <div className="mb-2">
+            You have a new order request{" "}
+            <span className="font-bold px-1"> #456758</span>
+          </div>
+          <div className="">12h ago</div>
+        </div>
+        <div className="">
+          <svg
+            width="8"
+            height="8"
+            viewBox="0 0 8 8"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect width="8" height="8" rx="4" fill="#020202" />
+          </svg>
+        </div>
+      </div>
+      <div className="w-full max-w-md py-3 px-8 rounded-lg border bg-white flex justify-between">
+        <div className="">
+          <div className="mb-2">
+            You have a new order request
+            <span className="font-bold px-1">#456758</span>
+          </div>
+          <div className=""> 12h ago</div>
+        </div>
+        <div className="">
+          <svg
+            width="8"
+            height="8"
+            viewBox="0 0 8 8"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect width="8" height="8" rx="4" fill="#020202" />
+          </svg>
+        </div>
+      </div>
+      <div className="w-full max-w-md py-3 px-8 rounded-lg border bg-white flex justify-between">
+        <div className="">
+          <div className="mb-2">
+            You have a new order request
+            <span className="font-bold px-1">#456758</span>
+          </div>
+          <div className=""> 12h ago</div>
+        </div>
+        <div className="">
+          <svg
+            width="8"
+            height="8"
+            viewBox="0 0 8 8"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect width="8" height="8" rx="4" fill="#020202" />
+          </svg>
+        </div>
+      </div>
+      <div className="w-full max-w-md py-3 px-8 rounded-lg border bg-white flex justify-between">
+        <div className="">
+          <div className="mb-2">
+            You have a new order request
+            <span className="font-bold px-1">#456758</span>
+          </div>
+          <div className=""> 12h ago</div>
+        </div>
+        <div className="">
+          <svg
+            width="8"
+            height="8"
+            viewBox="0 0 8 8"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect width="8" height="8" rx="4" fill="#020202" />
+          </svg>
+        </div>
+      </div>
+      <div className="w-full max-w-md py-3 px-8 rounded-lg border bg-white flex justify-between">
+        <div className="">
+          <div className="mb-2">
+            You have a new order request
+            <span className="font-bold px-1">#456758</span>
+          </div>
+          <div className=""> 12h ago</div>
+        </div>
+        <div className="">
+          <svg
+            width="8"
+            height="8"
+            viewBox="0 0 8 8"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect width="8" height="8" rx="4" fill="#020202" />
+          </svg>
+        </div>
+      </div>
+      <div className="w-full max-w-md py-3 px-8 rounded-lg border bg-white flex justify-between">
+        <div className="">
+          <div className="mb-2">
+            You have a new order request
+            <span className="font-bold px-1">#456758</span>
+          </div>
+          <div className=""> 12h ago</div>
+        </div>
+        <div className="">
+          <svg
+            width="8"
+            height="8"
+            viewBox="0 0 8 8"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect width="8" height="8" rx="4" fill="#020202" />
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
 }
